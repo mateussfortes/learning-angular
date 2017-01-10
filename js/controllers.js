fbd23daac0072668dce188ebe8e42defbee1a129
Mateus Fortes <mateussilveirafortes@gmail.com>
2017-01-09 22:33:21 -0200

    Hello World 2

---
+++ js/controllers.js
@@ -0,0 +1,28 @@
+
+'use strict';
+
+var helloWorldControllers = angular.module('helloWorldControllers', []);
+
+helloWorldControllers.controller(
+	'MainCtrl', 
+	[
+		'$scope', 
+		'$location', 
+		'$http', 
+		function MainCtrl($scope, $location, $http) {
+			$scope.message = "Hello World";
+		}
+	]
+);
+
+helloWorldControllers.controller(
+	'ShowCtrl', 
+	[
+		'$scope', 
+		'$location', 
+		'$http', 
+		function ShowCtrl($scope, $location, $http) {
+			$scope.message = "Hello World";
+		}
+	]
+);
